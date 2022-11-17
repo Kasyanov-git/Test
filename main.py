d7ec48b0f74d4bd70637a60b924d05d6114e1396
Kasyanov-git <64683843+Kasyanov-git@users.noreply.github.com>
2022-11-17 14:17:49 +0300

    Delete main.py

--- main.py
+++
@@ -1,7 +0,0 @@
-from fastapi import FastAPI
-from fastapi.responses import HTMLResponse
-
-app = FastAPI()
-@app.get("/url_name")
-async def root(name: str, message: str):
-    return HTMLResponse(f"<b>Привет от Никиты, {name}! <br> {message}</b>")
