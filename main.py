17acfbdab435e20dfef0c37631732185720f942f
Kasyanov-git <64683843+Kasyanov-git@users.noreply.github.com>
2022-11-17 14:13:28 +0300

    Update main.py

--- main.py
+++ main.py
@@ -4,4 +4,4 @@ from fastapi.responses import HTMLResponse
 app = FastAPI()
 @app.get("/url_name")
 async def root(name: str, message: str):
-    return HTMLResponse(f"<b>Hello {name}! <br> {message}</b>")
+    return HTMLResponse(f"<b>Привет от Никиты, {name}! <br> {message}</b>")
